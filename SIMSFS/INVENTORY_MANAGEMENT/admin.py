66f78fb1009270979dce82440ed0efb27b010653
Eldergod-McCullen <133879503+Eldergod-McCullen@users.noreply.github.com>
2026-01-05 20:14:46 +0300

    Update admin.py

--- SIMSFS/INVENTORY_MANAGEMENT/admin.py
+++ SIMSFS/INVENTORY_MANAGEMENT/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import ItemType,ItemCategory,ItemSubcategory,PaymentMode,County,Town,PaymentStatus,ReceiptStatus,ShippingStatus,UserRole
 from .models import Inventory,InventoryItem
-from .models import Supplier,PurchaseOrder,PurchaseDetail,Payment
+from .models import Supplier,PurchaseOrder,PurchaseDetail,Payment                                # IMPORT ALL MODEL CLASSES
 from .models import Customer,SalesOrder,SalesDetail,Receipt
 from .models import UserManager,User
 
