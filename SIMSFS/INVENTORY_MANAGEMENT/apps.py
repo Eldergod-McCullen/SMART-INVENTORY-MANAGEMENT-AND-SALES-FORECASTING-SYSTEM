3293229f2b571f11e70e98ff44dba93d0a078c06
Eldergod-McCullen <133879503+Eldergod-McCullen@users.noreply.github.com>
2026-01-06 21:56:35 +0300

    Update apps.py

--- SIMSFS/INVENTORY_MANAGEMENT/apps.py
+++ SIMSFS/INVENTORY_MANAGEMENT/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class InventoryManagementConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'INVENTORY_MANAGEMENT'
+    name = 'SIMSFS.INVENTORY_MANAGEMENT'
