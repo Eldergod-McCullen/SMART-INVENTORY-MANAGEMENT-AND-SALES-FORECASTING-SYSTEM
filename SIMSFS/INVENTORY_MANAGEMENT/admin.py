7f44db56d8e62c0a8efc1f84e6193e1e2abdafef
Eldergod-McCullen <133879503+Eldergod-McCullen@users.noreply.github.com>
2026-01-06 21:53:46 +0300

    Refactor admin registration for clarity

--- SIMSFS/INVENTORY_MANAGEMENT/admin.py
+++ SIMSFS/INVENTORY_MANAGEMENT/admin.py
@@ -9,10 +9,26 @@ from .models import UserManager,User
 # Register your models here.
 
 # FIRST REGISTERING THE REFERENCE/DIMENSION TABLES
-admin.site.register(ItemType,ItemCategory,ItemSubcategory,PaymentMode,County,Town,PaymentStatus,ReceiptStatus,ShippingStatus,UserRole)
+admin.site.register(ItemType)
+admin.site.register(ItemCategory)
+admin.site.register(ItemSubcategory)
+admin.site.register(PaymentMode)
+admin.site.register(County)
+admin.site.register(Town)
+admin.site.register(PaymentStatus)
+admin.site.register(ReceiptStatus)
+admin.site.register(ShippingStatus)
+admin.site.register(UserRole)
 
 # REGISTERING THE MAIN TABLES
-admin.site.register(Inventory,InventoryItem)
-admin.site.register(Supplier,PurchaseOrder,PurchaseDetail,Payment)
-admin.site.register(Customer,SalesOrder,SalesDetail,Receipt)
-admin.site.register(UserManager,User)
+admin.site.register(Inventory)
+admin.site.register(InventoryItem)
+admin.site.register(Supplier)
+admin.site.register(PurchaseOrder)
+admin.site.register(PurchaseDetail)
+admin.site.register(Payment)
+admin.site.register(Customer)
+admin.site.register(SalesOrder)
+admin.site.register(SalesDetail)
+admin.site.register(Receipt)
+admin.site.register(User)
