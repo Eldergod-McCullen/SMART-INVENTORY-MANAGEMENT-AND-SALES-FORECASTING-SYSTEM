9801b3ed5e1cd1a583801144ff592ed86b237ee8
Eldergod-McCullen <133879503+Eldergod-McCullen@users.noreply.github.com>
2026-01-06 21:45:33 +0300

    Update import paths and template directories in settings

--- SIMSFS/settings.py
+++ SIMSFS/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'INVENTORY_MANAGEMENT',
+    'SIMSFS.INVENTORY_MANAGEMENT',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'SIMSFS.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['INVENTORY_MANAGEMENT\Templates'],
+        'DIRS': [], # 'INVENTORY_MANAGEMENT\Templates'
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
